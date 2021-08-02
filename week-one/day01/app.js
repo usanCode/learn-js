b5db0001ee78f23a04e5d92dbe7c90505be83d4e
Carlos Fortes <cdfortes@gmail.com>
2021-08-02 20:19:08 -0100

    Create app.js

---
+++ week-one/day01/app.js
@@ -0,0 +1,122 @@
+/*
+
+01 - In this directory (day01), there is an empty index.html. 
+
+Do the following:
+
+- Declare a basic HTML markup on it;
+
+- Change the title to "Exercise 01";
+
+- And link this file, app.js, before closing the body tag. 
+
+*/
+
+/*
+
+02 - To make sure this file is actually linked, display the phrase
+
+"JS is Awesome!" in an alert in the browser. 
+
+*/
+
+// write your code here
+
+/*
+
+03 - Comment out the alert you wrote above using the one-line comment. 
+
+OBS: Do declare a variable in JS we can use var, let and const for constants, if you have doubts you can learn about in https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics
+
+For this exercises, declare a const "myBirthYear", which receives the year of your birth, with 4
+
+digits. E.g.: 1930. 
+
+*/
+
+// write your code here
+
+/*
+
+04 - Declare a const "currentYear", which receives the current year, with 4 digits.
+
+*/
+
+/*
+
+05 - Display the consts "myBirthYear" and "currentYear", side by side, in the browser console.
+
+*/
+
+// write your code here
+
+/*
+
+06 - Try it.
+
+- Declare a const "myAge", which gets the current year subtracted by the year of your
+
+birth. But without entering the numbers manually.
+
+You can use the minus sign (-) between the two values and check what
+
+it happens.
+
+- Display "myAge" on the console.
+
+*/
+
+// write your code here
+
+/*
+
+07 - Before the console.log below, declare a const "ten", which receives the number
+
+10 and:
+
+- Uncomment the console.log;
+
+- Replace the word "value1" with the current year plus 10;
+
+- Replace the word "valor2" with your age plus 10;
+
+- Numbers must not be entered manually;
+
+- Watch the result in the browser console.
+
+Don't be scared by the crases and dollar signs. We'll talk more about operations
+
+maths and strings (texts) coming soon =)
+
+*/
+
+// console.log(`In ${value1}, I'll be ${value2} years old.`)
+
+/*
+
+08 - Open the website below in the browser and do the following:
+
+http://whathappenedinmybirthyear.com/
+
+- Open the devtools console;
+
+- There will probably be some warnings on it. Press ctrl + L to clear it;
+
+- In the devtools console, declare a const "myBirthYear" that receives the year when
+
+you were born (4 digits) and press enter;
+
+- At the end of the 1st line of the code below, replace YOUR_YEAR_OF_BIRTH with the use
+
+from the const myBirthYear;
+
+- Paste the two lines of code into the console, press enter, close the console and
+
+see what happens.
+
+document.querySelector('#birthYear').value = YOUR_YEAR_OF_BIRTH
+
+document.querySelector('.formButton').click()
+
+*/ 
+
