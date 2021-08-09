a72f54a1ac6ea1889358192569981744e07de015
usanCode <60264357+usanCode@users.noreply.github.com>
2021-08-09 13:50:57 +0200

    Update app.js

--- week-two/app.js
+++ week-two/app.js
@@ -1,1 +1,111 @@
+/*
+01 - In the index.html of this directory (day-01):
+- Declare a basic HTML markup;
+- Link this file, app.js;
+- Start your local server and open the browser console.
+*/
 
+/*
+02 - Store an empty array in a let "brFoods";
+*/
+
+/*
+03 - Use the proper method to add the following 3 items to the array:
+- Drumstick
+- null
+- Brigadier
+- Display brFoods on the console.
+*/
+
+/*
+04 - Comment the console.log above and:
+- Through a value reassignment, make the 2nd item of the
+array "brFoods" (null) is replaced by "Cheese Bread";
+- Display only the 2nd item of this array in the console.
+*/
+
+/*
+05 - Comment the console.log above and:
+- Store in a const "foodsInfo" the following string: "So far, the
+array "brFoods" has ITEM_NUMBER items: ITEM_NAME_1,
+ITEM_NAME_2 and ITEM_NAME_3.";
+- Replace "NUMBER_DE_ITEMS" with the correct value, but without entering the
+number directly;
+- Replace the words "ITEM_NAME_X" with the correct information,
+but without typing the names of the foods directly;
+- Display the foodsInfo on the console.
+*/
+
+/*
+06 - Comment the console.log above and:
+- Store an array with the items "Chico" and "Zeca" in a const
+"maleDogNames";
+- Now, in a const "femaleDogNames", store an array with the
+"Bass" and "Matilda" items;
+- Declare a "dogNames" let that receives the arrays join
+"maleDogNames" and "femaleDogNames";
+- Display the "dogNames" on the console.
+*/
+
+/*
+07 - Comment the console.log above and:
+- Display a string with all array items on the console
+"dogNames" separated by comma and white space;
+- Use the proper method to do this;
+- The result must be: "name1, name2, name3, name4".
+*/
+
+/*
+08 - Comment the console.log above and:
+- Display the last item in the "dogNames" array on the console, but from
+so that if the number of items in the array increases or decreases,
+the last item remains displayed;
+- To test this logic, in the line above the console.log that
+you just wrote, remove the last item from the "dogNames" array
+using the proper method for this;
+- The console should continue to display the last item in the array, which
+now it's "Bass".
+*/
+
+/*
+09 - Comment the console.log above and:
+- Add the item "Nina" at the end of the "dogNames" array using
+the proper method for this;
+- Now, store in a const "initials" a template string with
+only the initial letters of the names of the "dogNames" array;
+- Example: ['Nala', 'Zé'] would result in a string 'NZ';
+- Do not type the letters directly;
+- Display the "initials" on the console.
+*/
+
+/*
+10 - Comment the console.log above and:
+- Display the "initials" on the console, but with all lowercase letters;
+- Use the proper method for this.
+*/
+
+/*
+11 - Comment the console.log above and:
+- Declare a const "dessert" and make it receive a template
+string containing the "co" part of the 1st item of the "dogNames" array, a
+part "ca" of the 2nd item and the "na" part of the 4th item;
+- The string must result in the word 'cocana', which must not be
+typed directly;
+- At the end of the string, have the letter "n" replaced by
+"d", using the method suitable for this;
+- Display the string below on the console, replacing "DESSERT" by
+constant you created.
+'DESSERT is a coconut-based sweet, traditional in Latin America and Angola.'
+*/
+
+/*
+12 - Comment the console.log above and:
+- Declare an array with the numbers 3, 5, and 7 and store it in a let
+"oddNumbers";
+- Display in the console the result of the 2nd item of the array cubed. O
+displayed value must be 125;
+- Between the declaration of "oddNumbers" and the display of the 2nd item of the array
+cubed, make the 2nd item of the array receive all the value it
+already have, another 4;
+- Now the result displayed on the console should be 729.
+*/
